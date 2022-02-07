0d68aeac3d822f6fe39a826b62e5aaf9bee1854f
DnyaneshwarKolhe <dnyaneshwarkolhe2017@gmail.com>
2022-02-07 14:20:34 +0530

    7th Jan session

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
+import BtnComp from './BtnComp';
 import MyComp from './MyComponent';
 
 function App() {
+  let recievedValue;
+  function recieveDefaultValue(value) {
+    console.log("In App", value)
+    recievedValue = value;
+  }
   return (
-   <div>
-     <MyComp>
-       
-     </MyComp>
-   </div>
+    <div>
+      <BtnComp recieveDefaultValue={recieveDefaultValue} />
+      <MyComp id="myComp" name="firstComponent" style={{ color: 'red' }} count={recievedValue}>
+        <div>Hello</div>
+        <div>World</div>
+        <div>Covid</div>
+      </MyComp>
+      <MyComp id="nameComp" name="Yogesh" style={{ color: 'yellow', backgroundColor: 'red' }} count={recievedValue} />
+    </div>
   );
 }
 
